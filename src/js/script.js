67ca55fed683ca5b8beaf06092cb9a5d88296223
Bogdan Mykhailov <bogdan_mykhailov@icloud.com>
2021-10-27 23:58:59 +0300

    optimisation

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,29 @@ const slider = tns({
     autoplay: false,
     controls: false,
     nav: false,
+    preventScrollOnTouch: 'auto',
+    responsive: {
+        320: {
+            items: 1,
+            nav: true,
+        },
+        575: {
+            items: 1,
+            nav: true,
+        },
+        767: {
+            items: 1,
+            nav: true,
+        },
+        991: {
+            items: 1, 
+            nav: true,        
+        },
+        1100: {
+            nav: false,
+        },
+        
+    }
 });
 
 document.querySelector('.prev').addEventListener ('click', function () {
